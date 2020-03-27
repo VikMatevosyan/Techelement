6dfb3fa3f66d7973d1ea5b23a35e6496c49af97d
Vik <matevosyan.vik@mail.ru>
2020-03-27 20:42:59 +0400

    Third stage

--- script.js
+++ script.js
@@ -1,67 +1,87 @@
-// Написать функцию, принимающую на вход количество пар скобок и возвращающую валидную строку, вида «({})()»,
-// в которой соблюдена правильная вложенность всех скобок.
-//     В строке должны присутствовать различные комбинации. Т.е. строка не может быть  всегда «((()))»
-// или «()()()». Следует комбинировать три вида скобок «()», «{}», «[]».
-
-function isBalanced(str) {
-    var br = "(){}[]";
-    var st = [];
-    for (var i = 0; i < str.length; ++i) {
-        var ch = str[i];
-        var ind = br.indexOf(ch);
-        if (ind >= 0) {
-            if (ind & 1) {
-                if (!st.length) return false;
-                var last_br = st.pop();
-                if (last_br != br[ind - 1]) return false;
-            } else {
-                st.push(ch);
-            }
-        }
-    }
-    return !st.length;
-}
-
-// Написать функцию, проверяющую валидность работы первой функции. Функция принимает на вход строку из задачи выше
-// и возвращает true || false. (есть ошибки вложенности или нет)
-
-// Добавить возможность изменить название товара. При клике на название, появляется поле редактирования,
-//     после всех изменений и нажатии кнопки «Готов», данные о новых именах товаров отправляются на сервер
 
 const cat = document.querySelector('.catalog-name');
-const menu = document.querySelector('.menu');
-cat.addEventListener('click', catalog);
-function catalog () {
-   menu.classList.toggle('block');
-}
+cat.onmouseover = function(event) {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('block');
+};
+
+// 1.При наведении мыши на данную область блочное представление товарных элементов должно замениться табличным.
+const right = document.querySelector('.right-span');
+right.onmouseover = function(event) {
+    const catalogs = document.querySelector('.catalog');
+    catalogs.classList.toggle('table');
+};
+
+
+// 2.Добавить возможность изменить название товара. При клике на название, появляется поле редактирования,
+//     после всех изменений и нажатии кнопки «Готов», данные о новых именах товаров отправляются на сервер
 
 $(document).ready(function () {
     $(".name").click(function () {
         const currentText = $(this);
         let text = currentText.text();
         let inp = currentText.closest(".nam").find(".name-change");
-        let btn = $('.ready-btn');
         inp.val(text);
-        currentText.addClass('none');
-        inp.addClass('block');
-        btn.addClass('block');
+        currentText.removeClass('block').addClass('none');
+        inp.addClass('block').removeClass('none');
+        $(inp).focus();
     });
     $(".name-change").blur(function () {
-        const currentText = $(this);
-        let inp = currentText.closest(".nam").find(".name-change");
-        currentText.text =  inp.val();
-        inp.toggleClass('none');
-        currentText.toggleClass('block');
-    })
+        const currentInput = $(this);
+        let span = currentInput.closest(".nam").find(".name");
+        span.text(currentInput.val());
+        currentInput.removeClass('block').addClass("none");
+        span.removeClass('none').addClass("block");
+    });
+    $('.ready-btn').click(function () {
+        let s = $('.name-change').serialize();
+        $.ajax({url:"", type:"post", data: s});
+    });
+
 });
 
 
+// 3.Добавить возможность перетаскивать товары мышкой, менять местами
+$( function() {
+    $('.cat').sortable();
+} );
 
-// $( function() {
-//     $( ".cat" ).sortable();
-//     $( ".cat" ).disableSelection();
-// } );
+// 4.Написать функцию, принимающую на вход количество пар скобок и возвращающую валидную строку, вида «({})()»,
+// в которой соблюдена правильная вложенность всех скобок.
+//     В строке должны присутствовать различные комбинации. Т.е. строка не может быть  всегда «((()))»
+// или «()()()». Следует комбинировать три вида скобок «()», «{}», «[]».
+// Написать функцию, проверяющую валидность работы первой функции. Функция принимает на вход строку из задачи выше
+// и возвращает true || false. (есть ошибки вложенности или нет)
 
+function correctBrackets(brs){
+    let opening = {
+        '[': ']',
+        '{': '}',
+        '(': ')',
+        '<': '>',
+    };
 
+    let stack = [];
+    let ret = '';
 
+    for(let i = 0; i < brs.length; i++){
+        let c = brs[i];
 
+        if(opening[c]){
+            stack.push(c);
+        } else {
+            if(stack.length === 0){
+                return null;
+            }
+
+            let br = stack.pop();
+
+            c = opening[br];
+        }
+
+        ret += c;
+    }
+    if (stack.length > 0){
+        return null;
+    }
+}
